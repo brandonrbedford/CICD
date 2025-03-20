061fcce2a10937faf5e10d162e010f1c76d4417d
Brandon <brandonrbedford@gmail.com>
2025-03-20 15:47:08 -0500

    Test Script with Batfish

--- batfishcheck.py
+++ batfishcheck.py
@@ -1,4 +1,5 @@
 from pybatfish.client.session import Session
 
-bf = Session(host="localhost")  # Connect to the local Batfish server
-print(bf)
+#bf = Session(host="localhost")  # Connect to the local Batfish server
+print("bf")
+
